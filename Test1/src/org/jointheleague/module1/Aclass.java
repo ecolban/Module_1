b8f03f1d0f7c48cd36ce408bc0cd60e955ac77d3
Erik Colban <erik.colban@jointheleague.org>
2016-10-11 22:41:36 -0700

    Add initial

---
+++ Test1/src/org/jointheleague/module1/Aclass.java
@@ -0,0 +1,5 @@
+package org.jointheleague.module1;
+
+public class Aclass {
+
+}
---
+++ Test2/src/org/jointheleague/module1/Bclass.java
@@ -0,0 +1,13 @@
+package org.jointheleague.module1;
+
+public class Bclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
